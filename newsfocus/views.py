5a8f3a5bb237279ba66c3087bc9950ff5b592210
xihe92 <xihesongruihua@hotmail.com>
2016-05-11 05:04:56 -0400

    result page search finish

--- newsfocus/views.py
+++ newsfocus/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from search_news import searchByCategory
+from search_news import searchByCategory, niceSearch
 from django.http import JsonResponse
 
 # Create your views here.
@@ -11,7 +11,13 @@ def results(request):
 	return render(request, 'newsfocus/results.html', {})
 
 def ordinary_search(request):
-    print "12312312321321312321321321"
     keywords = request.POST.get("keywords")
-    result = searchByCategory(keywords)
+    result = niceSearch(keywords=keywords)
+    return JsonResponse(result, safe=False)
+
+def advanced_search(request):
+    keywords = request.POST.get("keywords")
+    daterange = request.POST.get("daterange")
+    categories = request.POST.getlist("categories[]")
+    result = niceSearch(keywords=keywords, ctg=categories)
     return JsonResponse(result, safe=False)
